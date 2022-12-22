3f2e70b6eb835017de0d20ec4fb003e76b731a3b
BCFZzw <zhiwei.zhang@mail.mcgill.ca>
2022-12-21 23:03:00 -0500

    remove dependencies on parsing

--- pastCode/windowStats.py
+++ pastCode/windowStats.py
@@ -1,8 +1,41 @@
 import numpy as np
 import moments.LD
-import parsing as ps
 
+def segmentation(allele, distanceMatrix, maxLen, boundaries):
+    """
+    Separate a given allele matrix into equal genomic distance segments
+    @param: allele {np.narray}. "calldata/GT" matrix extracted from scikit allele of a vcf
+    @param: distanceMatrix. {np.narray} "variants/POS" matrix extracted from scikit allele of a vcf 
+    @param: maxLen {int}. the maximum length from a vcf file
+    @param: boudnaries {int}. the intended separation segments of the genotypes given allele's genomic region
+    @returns segment {np.narray} [shape=(boundaries, _, indNum, 2)], segmentName {np.array} [shape=boundaries]. return the moment accepted segment format and associated names in two arrays. 
+    """
+    segment = np.empty(dtype = "O", shape = boundaries)
+    #segmentName = np.empty(dtype = ("str", len(str(boundaries))+1), shape = boundaries) #the str storage size determined by the length of boundaries, boundaries = 100 => str lenght of 3, +1 for R at the beginning
+    for i in range(1, boundaries + 1):
+        if i == 1:
+            index = distanceMatrix < i*maxLen/boundaries
+        elif i == boundaries:
+            index = distanceMatrix > (i-1)*maxLen/boundaries
+        else:
+            index = (distanceMatrix > (i-1)*maxLen/boundaries) & (distanceMatrix < i*maxLen/boundaries)
+        
+        segment[i-1] = (allele[index, :])
+        #segmentName[i-1] = "R"+str(i)
+    return segment#, segmentName
 
+def readVcf(fileName, inputPath): #checked
+    """
+    read a single vcf (with input directory or not, if not inputPath = "")
+    @param fileName {string .trees}
+    @param inputPath {string}
+    @return {np.narray two fields}
+    @TODO: extract contig length, slightly hard and might require hardcoding
+    """
+    read = allel.read_vcf(inputPath + fileName, fields = ["variants/POS", "calldata/GT"])
+    distance = read["variants/POS"]
+    momentGenotype = shapeTransform(read["calldata/GT"])
+    return distance, momentGenotype
             
 def binStats(segment):
     """
@@ -17,7 +50,6 @@ def binStats(segment):
     #name = np.empty(shape = size, dtype = ("str", 10))
     for i in range(0, size):
         #TODO change to parsing
-        #region = ps.shapeTransform(segment[i])
         region = segment[i]
         stats = moments.LD.Parsing.compute_pairwise_stats(region, genotypes = True)
         
@@ -39,13 +71,12 @@ def allBinStats(vcf_list, inputPath, outputPath, maxLen = 1e7, outName = "window
     Process binStats for all vcfs. Save format may be improved?
     """
     length = len(vcf_list)
-    #distanceList, genotypeList = ps.readVcfs(vcf_list, inputPath)
     resultList = np.empty(dtype = "O", shape = length)
     
     for i in range(0, length):
-        distance, genotype = ps.readVcf(vcf_list[i], inputPath)
-        segment, segmentName = ps.segmentation(genotype, distance, maxLen, boundaries)
-        result =  binStats(segment, segmentName)
+        distance, genotype = readVcf(vcf_list[i], inputPath)
+        segment = segmentation(genotype, distance, maxLen, boundaries)
+        result =  binStats(segment)
         resultList[i] = result
     
 
