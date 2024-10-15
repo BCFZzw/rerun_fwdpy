68512772d8ab6e708a0bdfd609f3dff19bf82729
BCFZzw <zhiwei.zhang@mail.mcgill.ca>
2024-10-15 16:51:07 -0400

    maybe adding an assert function in Parsing?

--- cython_filter_loop/test_modified_moments_LD.py
+++ cython_filter_loop/test_modified_moments_LD.py
@@ -17,6 +17,32 @@ class Test_simple_constrianed_genotype_count(unittest.TestCase):
             threshold = 2
             moments.LD.Parsing.compute_pairwise_stats(self.G, pos_array, genotypes = True, distance_constrained = threshold)
 
+    def test_none_filtered(self):
+        pos_array = np.array([1, 3, 5, 7, 9], dtype = np.int32)
+        threshold = 1
+        D2_pw, Dz_pw, pi2_pw, D_pw = moments.LD.Parsing.compute_pairwise_stats(self.G, pos_array, genotypes = True, distance_constrained = threshold)
+        self.assertTrue(np.all(D2_pw == self.D2_pw))
+        self.assertTrue(np.all(Dz_pw == self.Dz_pw))
+        self.assertTrue(np.all(D_pw == self.D_pw))
+        self.assertTrue(np.all(pi2_pw == self.pi2_pw))
+    
+    def test_not_constrained(self):
+        pos_array = np.array([1, 2, 3, 4, 5], dtype = np.int32)
+        threshold = 0
+        D2_pw, Dz_pw, pi2_pw, D_pw = moments.LD.Parsing.compute_pairwise_stats(self.G, pos_array, genotypes = True, distance_constrained = threshold)
+        self.assertTrue(np.all(D2_pw == self.D2_pw))
+        self.assertTrue(np.all(Dz_pw == self.Dz_pw))
+        self.assertTrue(np.all(D_pw == self.D_pw))
+        self.assertTrue(np.all(pi2_pw == self.pi2_pw))
+    
+    def test_all_filtered(self):
+        pos_array = np.array([1, 2, 3, 4, 5], dtype = np.int32)
+        threshold = 10
+        D2_pw, Dz_pw, pi2_pw, D_pw = moments.LD.Parsing.compute_pairwise_stats(self.G, pos_array, genotypes = True, distance_constrained = threshold)
+        self.assertTrue(np.all(D2_pw == None))
+        self.assertTrue(np.all(Dz_pw == None))
+        self.assertTrue(np.all(D_pw == None))
+        self.assertTrue(np.all(pi2_pw == None))
             
 if __name__ == '__main__':
     unittest.main()
