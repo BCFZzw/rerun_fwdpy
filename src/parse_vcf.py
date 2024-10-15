cf0f7e0842a6bb36f7815e25c8b7f5547935e881
BCFZzw <zhiwei.zhang@mail.mcgill.ca>
2024-10-15 18:00:48 -0400

    fixing errors in panel reading

--- src/parse_vcf.py
+++ src/parse_vcf.py
@@ -4,26 +4,28 @@ import zarr
 
 def _read_panel(panel_file):
     """
-    Read panel list, expect space-delimited file and the headers "SampleID", "Population", "Superpopulation" present.
+    Read panel list, expect tab-delimited file and the headers "sample", "pop", "super_pop" present.
     """
-    panel_df = pd.read_csv(panel_file, sep = " ")
-    assert ["SampleID", "Population", "Superpopulation"] in panel_df.columns
+    panel_df = pd.read_csv(panel_file, sep = "\t")
+    assert "sample" in panel_df.columns
+    assert "pop" in panel_df.columns
+    assert "super_pop" in panel_df.columns
     return panel_df
 
-def locate_sample_individuals(callset, panel_file, population = None, superpopulation = None):
+def locate_sample_individuals(callset, panel_file, pop = None, super_pop = None):
     """
     Locate individuals specified in the panel files in the VCF callset data. Individuals belonging to a specific population or superpopulation can be sampled.
     """
     panel_df = _read_panel(panel_file)
-    samples_list = list(samples) 
-    if (population is not None) and (superpopulation is not None):
+    samples_list = list(callset["samples"][:]) 
+    if (pop is not None) and (super_pop is not None):
         raise ValueError("One of population or superpopulation can be specified. Not Both.")
-    if (population is not None):
-        panel_df = panel_df[panel_df.Population == population]
-    if (superpopulation is not None):
-        panel_df = panel_df[panel_df.Superpopulation == superpopulation]
+    if (pop is not None):
+        panel_df = panel_df[panel_df.pop == pop]
+    if (super_pop is not None):
+        panel_df = panel_df[panel_df.super_pop == super_pop]
     assert len(panel_df) > 0
-    samples_callset_index = np.where(np.in1d(samples_list, panel_df.SampleID))[0]
+    samples_callset_index = np.where(np.in1d(samples_list, panel_df.sample))[0]
     ### no one missing
     assert -1 not in samples_callset_index
     panel_df['callset_index'] = samples_callset_index
--- src/test/test_parse_vcf.py
+++ src/test/test_parse_vcf.py
@@ -1,3 +1,23 @@
 import unittest
+import zarr
+import sys
+sys.path.insert(1, '/home/alouette/projects/ctb-sgravel/alouette/Dz_sweep_final/simulation_review/src')
+from parse_vcf import *
 
 
+class Test_parse_vcf(unittest.TestCase):
+
+    panel_file = "/home/alouette/projects/ctb-sgravel/alouette/1000Genome/population_panel/integrated_call_samples_v3.20130502.2504.ALL.panel"
+    zarr_path = "/home/alouette/projects/ctb-sgravel/data/30x1000G_biallelic_strict_masked/zarrFormat/chr22"
+    callset = zarr.open_group(zarr_path, mode='r')
+
+    def test_catch_pop_superpop_both_specified(self):
+        with self.assertRaises(ValueError):
+            locate_sample_individuals(self.callset, self.panel_file, pop = "GBR", super_pop = "EUR")
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+            
