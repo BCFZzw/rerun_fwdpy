f8f7311461a8d7ab8de65977e146bcad0b9d8611
BCFZzw <zhiwei.zhang@mail.mcgill.ca>
2024-10-16 11:22:21 -0400

    working, updating to string to select columns

--- src/parse_vcf.py
+++ src/parse_vcf.py
@@ -1,6 +1,7 @@
 import pandas as pd 
 import allel
 import zarr
+import numpy as np
 
 def _read_panel(panel_file):
     """
@@ -12,7 +13,7 @@ def _read_panel(panel_file):
     assert "super_pop" in panel_df.columns
     return panel_df
 
-def locate_sample_individuals(callset, panel_file, pop = None, super_pop = None):
+def locate_panel_individuals(callset, panel_file, pop = None, super_pop = None):
     """
     Locate individuals specified in the panel files in the VCF callset data. Individuals belonging to a specific population or superpopulation can be sampled.
     """
@@ -21,11 +22,12 @@ def locate_sample_individuals(callset, panel_file, pop = None, super_pop = None)
     if (pop is not None) and (super_pop is not None):
         raise ValueError("One of population or superpopulation can be specified. Not Both.")
     if (pop is not None):
-        panel_df = panel_df[panel_df.pop == pop]
+        panel_df = panel_df[panel_df["pop"] == pop]
     if (super_pop is not None):
-        panel_df = panel_df[panel_df.super_pop == super_pop]
-    assert len(panel_df) > 0
-    samples_callset_index = np.where(np.in1d(samples_list, panel_df.sample))[0]
+        panel_df = panel_df[panel_df["super_pop"] == super_pop]
+    if len(panel_df) == 0:
+        raise ValueError("Sampled population/superpopulation not found in panel.")
+    samples_callset_index = np.where(np.in1d(samples_list, panel_df["sample"]))[0]
     ### no one missing
     assert -1 not in samples_callset_index
     panel_df['callset_index'] = samples_callset_index
--- src/test/test_parse_vcf.py
+++ src/test/test_parse_vcf.py
@@ -3,6 +3,7 @@ import zarr
 import sys
 sys.path.insert(1, '/home/alouette/projects/ctb-sgravel/alouette/Dz_sweep_final/simulation_review/src')
 from parse_vcf import *
+import pandas as pd
 
 
 class Test_parse_vcf(unittest.TestCase):
@@ -10,10 +11,21 @@ class Test_parse_vcf(unittest.TestCase):
     panel_file = "/home/alouette/projects/ctb-sgravel/alouette/1000Genome/population_panel/integrated_call_samples_v3.20130502.2504.ALL.panel"
     zarr_path = "/home/alouette/projects/ctb-sgravel/data/30x1000G_biallelic_strict_masked/zarrFormat/chr22"
     callset = zarr.open_group(zarr_path, mode='r')
+    panel_df = pd.read_csv(panel_file, sep = "\t")
 
     def test_catch_pop_superpop_both_specified(self):
         with self.assertRaises(ValueError):
-            locate_sample_individuals(self.callset, self.panel_file, pop = "GBR", super_pop = "EUR")
+            locate_panel_individuals(self.callset, self.panel_file, pop = "GBR", super_pop = "EUR")
+
+
+    def test_catch_pop_not_exist(self):
+        with self.assertRaises(ValueError):
+            locate_panel_individuals(self.callset, self.panel_file, pop = "ABE")
+            
+
+    def test_subset_AFR(self):
+        loc_samples = locate_panel_individuals(self.callset, self.panel_file, super_pop = "AFR")
+        self.assertTrue(len(loc_samples) == sum(self.panel_df.super_pop == "AFR"))
 
 
 
