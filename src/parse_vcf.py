987e16bba8559f8ba96d7dfbea9d0937b0bdc4da
BCFZzw <zhiwei.zhang@mail.mcgill.ca>
2024-11-25 12:05:44 -0500

    splitted sample_variants into several small functions

--- src/parse_vcf.py
+++ src/parse_vcf.py
@@ -3,7 +3,7 @@ import allel
 import zarr
 import numpy as np
 
-def _read_panel(panel_file):
+def _read_panel(panel_file) -> pd.DataFrame :
     """
     Read panel list, expect tab-delimited file and the headers "sample", "pop", "super_pop" present.
     """
@@ -13,17 +13,23 @@ def _read_panel(panel_file):
     assert "super_pop" in panel_df.columns
     return panel_df
 
-def get_subpopulations(panel_file, super_pop):
+def get_pops_from_superpop(panel_file, super_pop) -> np.array :
+    """
+    List all populations within a superpopulation.
+    """
     panel_df = _read_panel(panel_file)
     assert super_pop in panel_df["super_pop"].unique()
     return panel_df[panel_df.super_pop == super_pop]["pop"].unique()
 
 def locate_jackknife_individuals(callset_samples, panel_file, super_pop, jackknife_pop):
+    """
+    Locate the indices of leave-one-population-out individuals. 
+    Used in jackknife estimation.
+    """
     loc_super_pop_samples = locate_panel_individuals(callset_samples, panel_file, super_pop = super_pop)
     loc_pop_samples = locate_panel_individuals(callset_samples, panel_file, pop = jackknife_pop)
     loc_jackknife = np.setxor1d(loc_super_pop_samples, loc_pop_samples, assume_unique = False)
     assert len(loc_super_pop_samples) - len(loc_pop_samples) == len(loc_jackknife)
-    assert len(loc_jackknife) > 0
     return loc_jackknife
 
 
@@ -42,7 +48,7 @@ def locate_panel_individuals(callset_samples, panel_file, pop = None, super_pop 
     if len(panel_df) == 0:
         raise ValueError("Sampled population/superpopulation not found in panel.")
     samples_callset_index = np.where(np.in1d(samples_list, panel_df["sample"]))[0]
-    ### no one missing
+    ### If missing
     if -1 in samples_callset_index:
         raise ValueError("Some panel individuals are missing in the vcf data.")
     panel_df['callset_index'] = samples_callset_index
@@ -77,26 +83,55 @@ def _read_zarr_callset(zarr_path):
     callset_samples = callset["samples"]
     return callset, genotype_zarr, callset_pos, callset_samples
 
+def select_variants(genotype_dask, pos_start, pos_end):
+    """
+    Subset genotype based on given positions.
+    """
+    loc_region = locate_genotype_region(pos_array, pos_start, pos_end)
+    pos_array_var = pos_array[loc_region]
+    ### when using indices: take, when using boolean: compress
+    genotype_var = genotype_dask.take(loc_region, axis = 0)
+    return genotype_var, pos_array_var
+
+def select_sample(genotype_dask, callset_samples, panel_file, pop = None, super_pop = None):
+    """
+    Subset genotype based on a given population or superpopulation.
+    """
+    loc_samples = locate_panel_individuals(callset_samples, panel_file, pop, super_pop)
+    genotype_dask = genotype_dask.take(loc_samples, axis = 1)
+    genotype_sample = genotype_dask.take(loc_samples, axis = 1)
+    return genotype_sample
+
+def select_sample_jackknife(genotype_dask, callset_samples, panel_file, super_pop, jackknife_pop):
+    """
+    For Jackknife, subsect genotype based on leave-one-out populations from a superpopulation.
+    """
+    loc_jackknife = locate_jackknife_individuals(callset_samples, panel_file, super_pop = super_pop, jackknife_pop = jackknife_pop)
+    genotype_jackknife = genotype_dask.take(loc_samples, axis = 1)
+    return genotype_jackknife
+
+
 def select_sample_variants(zarr_path, pos_start = None, pos_end = None, panel_file = None, pop = None, super_pop = None, jackknife_pop = None):
+    """
+    Return the genotype array of selected samples and individuals.
+    Either population or superpopulation can be specified.
+    """
     callset, genotype_zarr, callset_pos, callset_samples = _read_zarr_callset(zarr_path)
     pos_array = allel.SortedIndex(callset_pos)
     genotype_dask = allel.GenotypeDaskArray(genotype_zarr)
 
     if (pos_start is not None) or (pos_end is not None):
-        loc_region = locate_genotype_region(pos_array, pos_start, pos_end)
-        pos_array = pos_array[loc_region]
-        ### when using indices: take, when using boolean: compress
-        genotype_dask = genotype_dask.take(loc_region, axis = 0)
+        genotype_dask, pos_array = select_variants(genotype_dask, pos_start, pos_end)
 
     if panel_file is not None:
         if jackknife_pop is not None:
             assert super_pop is not None
-            loc_jackknife = locate_jackknife_individuals(callset_samples, panel_file, super_pop = super_pop, jackknife_pop = jackknife_pop)
-            genotype_dask = genotype_dask.take(loc_jackknife, axis = 1)
-        else:
-            loc_samples = locate_panel_individuals(callset_samples, panel_file, pop, super_pop)
-            genotype_dask = genotype_dask.take(loc_samples, axis = 1)
-
+            genotype_dask = select_sample_jackknife(genotype_dask, callset_samples, panel_file, super_pop, jackknife_pop)
+        elif (super_pop is not None) or (pop is not None):
+            genotype_dask = select_sample(genotype_dask, callset_samples, panel_file, pop = pop, super_pop = super_pop)
+    elif (jackknife_pop is not None) or (super_pop is not None) or (pop is not None):
+        raise ValueError("Panel file is not given when selecting individuals.")
+    
     return genotype_dask, pos_array
 
 
