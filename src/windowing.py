c86f5b2f2f33509fdbc61b9339ef50816c824636
BCFZzw <zhiwei.zhang@mail.mcgill.ca>
2025-04-05 16:55:31 -0400

    finished windowing and add a function that can seleect a region to compute windows

--- src/windowing.py
+++ src/windowing.py
@@ -3,7 +3,7 @@ import msprime as ms
 import numpy as np
 
 
-def msprime_read_HapMap(path: str, pos_col = 1, rate_col = 2) -> ms.RateMap:
+def msprime_read_HapMap(path: str) -> ms.RateMap:
     """
     Require msprime >= 1.0.0
     Read the recombination map using msprime functionality.
@@ -13,7 +13,7 @@ def msprime_read_HapMap(path: str, pos_col = 1, rate_col = 2) -> ms.RateMap:
     The 2 columns must be Position(bp), and Rate(cM/Mb). Default for them to take the second and third columns. 
     The return will be a msprime RateMap object, with position in bp, and rate in M/bp.
     """
-    ms_RateMap = ms.RateMap.read_hapmap(path, position_col= pos_col , rate_col = rate_col)
+    ms_RateMap = ms.RateMap.read_hapmap(path, position_col= 1 , rate_col = 2)
     return ms_RateMap
 
 
@@ -37,32 +37,27 @@ def get_cum_cM_from_bp(pos_list: list, ms_RateMap: ms.RateMap) -> np.ndarray:
     return ms_RateMap.get_cumulative_mass(pos_list)*100
 
 
-
-
-def window_by_reombination(snp_array: list, pos_array: list, rec_map: ms.RateMap, rec_start: float, rec_end: float, rec_step = 0.04) -> pd.DataFrame:
+def window_by_recombination(rec_map_path: str, rec_step = 0.04, pos_start = None, pos_end = None) -> pd.DataFrame:
     """
-    Require msprime >= 1.0.0 to use RateMap
-    Call this function to get the window list separated by rec_distance
-    How to handle the last position if rec subsetting is not possible?
+    Return a dataframe of non-overlapping windows of fixed recombination rate calculated from the provided rate map. Units are in centi-Morgan.
+    The user can specify a position start and end to calculate the windows.
+    The first window will be equal to the rate map start or position start, whichever is larger.
+    The last window will be last full-length window of rec_step size before the rate map end or position end.
     """
-    ### end has to be larger than start
-    if np.any(rec_start < rec_end):
-        raise ValueError(f"Input recombination range is invalid")
-    ### if given rec distance is smaller or equal to the the step 
-    ### return the original array without subsetting
-    if rec_end - rec_start <= rec_step:
-        rec_list = [rec_start, rec_end]                    
-    else:
-        rec_list = np.arange(rec_start, rec_end, rec_step)
-        ### check the last position
-        ### floating point issue, the last position can be larger than end
-        if rec_list[-1] > rec_end:
-            rec_list[-1] = rec_end
+    ms_RateMap = msprime_read_HapMap(rec_map_path)
+    if (pos_start is None):
+        pos_start = 0
+    if (pos_end is None):
+        pos_end = ms_RateMap.sequence_length
+    if pos_end < pos_start:
+        raise ValueError(f"Input positions are invalid")
+    trim_RateMap = ms_RateMap.slice(pos_start, pos_end, trim = True)
+    ### generate fixed rec rate windows
+    cM_list = np.arange(0, trim_RateMap.total_mass * 100, rec_step)
     ### get bp positions
-    bp_array = _get_bp_from_cum_cM(rec_list)
-    ### call _windowing function
-    window_list = _scikit_allel_windowing(snp_array, pos_array, bp_array)
-    return window_list
+    bin_list = get_bp_from_cum_cM(cM_list, trim_RateMap)
+    window_df = pd.DataFrame({"window_start": bin_list[:-1], "window_end": bin_list[1:], "start_cM": cM_list[:-1]})
+    return window_df
     
 
 def window_to_bed(savePath: str, window_df: pd.DataFrame, chromosome = "."):
